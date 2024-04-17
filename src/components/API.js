cc8bfc296a7b1fa61fed5bf8d879152b72d6ee0d
Dmytro Kasko <dimoks77@gmail.com>
2024-04-17 17:16:37 +0200

    formik

--- src/components/API.js
+++ src/components/API.js
@@ -5,7 +5,7 @@ export async function fetchData(query, page, setPhotos, setLoading, setError) {
         setLoading(true);
         const response = await axios.get(`https://api.unsplash.com/search/photos/`, {
             params: {
-                client_id: '4Ha1lW10vYE2G9EauE11B4hkWzNMh1SuojsbWDZ2Kl8',
+                client_id: 'sSw2A24lCtgMKKWaGbVZZ3GjpqcpzhpoZDxANpcbn30',
                 query: query,
                 per_page: 12,
                 page: page,
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,44 +1,38 @@
 import css from "./SearchBar.module.css";
 import { IoIosSearch } from "react-icons/io";
-import { Field, Form, Formik } from "formik";
-import { useState } from "react";
-import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import { Field, Form, Formik, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 export const SearchBox = ({ onSearch }) => {
-    const [error, setError] = useState("");
+  const SignupSchema = Yup.object().shape({
+    query: Yup.string().required("Enter text for search!"),
+  });
 
-    const handleSubmit = (values, { resetForm }) => {
-        if (!values.query) {
-            setError("Enter text for search");
-        } else {
-            onSearch(values.query);
-            resetForm();
-            setError(""); 
-        }
-    };
+  const handleSubmit = (values, { resetForm }) => {
+    onSearch(values.query);
+    resetForm();
+  };
 
-    return (
-        <>
-            <header className={css.header}>
-                <Formik initialValues={{ query: "" }} onSubmit={handleSubmit}>
-                    <Form className={css.form}>
-                        <Field
-                            className={css.input}
-                            autoComplete="off"
-                            autoFocus
-                            type="text"
-                            name="query"
-                            placeholder="Search images with photos"
-                        />
-                        <button type="submit" className={css.btn}>
-                            <IoIosSearch className={css.icon} />
-                        </button>
-                    </Form>
-                </Formik>
-            </header>
-            <ErrorMessage error={error} clearError={() => setError("")} /> 
-        </>
-    );
+  return (
+    <header className={css.header}>
+      <Formik initialValues={{ query: "" }} validationSchema={SignupSchema} onSubmit={handleSubmit}>
+        <Form className={css.form}>
+          <Field
+            className={css.input}
+            autoComplete="off"
+            autoFocus
+            type="text"
+            name="query"
+            placeholder="Search images with photos"
+          />
+          <ErrorMessage name="query" render={(msg) => <div className={css.error}>{msg}</div>} />
+          <button type="submit" className={css.btn}>
+            <IoIosSearch className={css.icon} />
+          </button>
+        </Form>
+      </Formik>
+    </header>
+  );
 };
 
 export default SearchBox;
